f2fb60ba9c63aa00c30ad3d730fdb1a942a8413b
Garrett Knox <garrettknox@Garretts-MacBook-Pro-2.local>
2018-04-14 16:17:52 -0500

    Handle missing Thumbnail
    Update made to Book component to account for books with no imageLinks
    attribute

--- src/Book.js
+++ src/Book.js
@@ -8,7 +8,7 @@ class Book extends Component {
 
 			<div className="book">
         <div className="book-top">
-          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+          <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks && (this.props.book.imageLinks.thumbnail)})` }}></div>
           <div className="book-shelf-changer">
             <select value={this.props.book.shelf} onChange={(event) => this.props.updateBook(this.props.book, event.target.value)}>
               <option value="none" disabled>Move to...</option>
