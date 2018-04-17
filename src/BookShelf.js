cfd32938930cb8758b5f63497c16d808de758180
Garrett Knox <garrettknox@garretts-mbp-2.attlocal.net>
2018-04-16 21:24:39 -0500

    Convert BookShelf to Functional Component
    Since the BookShelf component was not utilizing a local state and only
    needed the render function defined, it was suggested that the component
    be converted to a stateless, functional component

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,38 +1,32 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import Shelf from './Shelf'
 
-class BookShelf extends Component {
-
-  static propTypes = {
-    title: PropTypes.string,
-    books: PropTypes.array,
-    updateBook: PropTypes.func
-  }
-
-	render() {
-
-		return (
-
-			<div className="list-books">
-        <div className="list-books-title">
-          <h1>{this.props.title}</h1>
-        </div>
-        <div className="list-books-content">
-          <div>
-            <Shelf title={"Currently Reading"} books={this.props.books} updateBook={this.props.updateBook} />
-            <Shelf title={"Want to Read"} books={this.props.books} updateBook={this.props.updateBook} />
-            <Shelf title={"Read"} books={this.props.books} updateBook={this.props.updateBook} />
-          </div>
-        </div>
-        <div className="open-search">
-          <Link to="/search">Add a book</Link>
+function BookShelf(props) {
+  return (
+    <div className="list-books">
+      <div className="list-books-title">
+        <h1>{props.title}</h1>
+      </div>
+      <div className="list-books-content">
+        <div>
+          <Shelf title={"Currently Reading"} books={props.books} updateBook={props.updateBook} />
+          <Shelf title={"Want to Read"} books={props.books} updateBook={props.updateBook} />
+          <Shelf title={"Read"} books={props.books} updateBook={props.updateBook} />
         </div>
       </div>
+      <div className="open-search">
+        <Link to="/search">Add a book</Link>
+      </div>
+    </div>
+  )
+}
 
-		);
-	}
+BookShelf.propTypes = {
+  title: PropTypes.string,
+  books: PropTypes.array,
+  updateBook: PropTypes.func
 }
 
 export default BookShelf
